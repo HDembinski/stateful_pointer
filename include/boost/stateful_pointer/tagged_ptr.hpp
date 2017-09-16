6fd7a86fa9d1b4b150ff3a1145e0bbbaf5e4b8dc
Hans Dembinski <hans.dembinski@gmail.com>
2017-09-16 15:59:22 +0200

    respect alignment of pointee, test up and downcasts

--- include/boost/stateful_pointer/tagged_ptr.hpp
+++ include/boost/stateful_pointer/tagged_ptr.hpp
@@ -2,6 +2,7 @@
 #define BOOST_STATEFUL_POINTER_TAGGED_PTR_HPP
 
 #include "boost/align/aligned_alloc.hpp"
+#include "boost/align/alignment_of.hpp"
 #include "boost/assert.hpp"
 #include "boost/cstdint.hpp"
 #include "boost/type_traits.hpp"
@@ -10,10 +11,13 @@ namespace boost {
 namespace stateful_pointer {
 
 namespace detail {
+constexpr uintptr_t max(uintptr_t a, uintptr_t b) { return a > b ? a : b; }
 constexpr unsigned pow2(unsigned n) noexcept {
   return n > 0 ? 2 * pow2(n - 1) : 1;
 }
-constexpr uintptr_t make_ptr_mask(unsigned n) noexcept { return ~uintptr_t(0) << n; }
+constexpr uintptr_t make_ptr_mask(unsigned n) noexcept {
+  return ~uintptr_t(0) << n;
+}
 }
 
 template <typename Tpointee, unsigned Nbits> class tagged_ptr {
@@ -25,18 +29,20 @@ public:
   using reference = Tpointee &;
   using const_reference = Tpointee const &;
 
-  constexpr tagged_ptr() noexcept : ptr(0) {}
+  constexpr tagged_ptr() noexcept : value(0) {}
 
   // no copy
   tagged_ptr(const tagged_ptr &) = delete;
   tagged_ptr &operator=(const tagged_ptr &) = delete;
 
-  tagged_ptr(tagged_ptr &&other) noexcept : ptr(other.ptr) { other.ptr = 0; }
+  tagged_ptr(tagged_ptr &&other) noexcept : value(other.value) {
+    other.value = 0;
+  }
 
   tagged_ptr &operator=(tagged_ptr &&other) noexcept {
     if (this != &other) {
-      ptr = other.ptr;
-      other.ptr = 0;
+      value = other.value;
+      other.value = 0;
     }
     return *this;
   }
@@ -46,8 +52,9 @@ public:
             typename = typename enable_if_c<
                 !(is_array<Upointee>::value) &&
                 is_convertible<Upointee *, Tpointee *>::value>::type>
-  tagged_ptr(tagged_ptr<Upointee, Nbits> &&other) noexcept : ptr(other.ptr) {
-    other.ptr = 0;
+  tagged_ptr(tagged_ptr<Upointee, Nbits> &&other) noexcept
+      : value(other.value) {
+    other.value = 0;
   }
 
   /// move assignment that allows conversion between base and derived
@@ -57,51 +64,51 @@ public:
                 is_convertible<Upointee *, Tpointee *>::value>::type>
   tagged_ptr &operator=(tagged_ptr<Upointee, Nbits> &&other) noexcept {
     if (this != &other) {
-      ptr = other.ptr;
-      other.ptr = 0;
+      value = other.value;
+      other.value = 0;
     }
     return *this;
   }
 
   ~tagged_ptr() {
-    auto p = get();
-    if (p) {
-      p->~Tpointee();
-      alignment::aligned_free(p);
+    auto tp = get();
+    if (tp) {
+      tp->~Tpointee();
+      alignment::aligned_free(tp);
     }
   }
 
   /// get all bits as integral type
-  bits_type bits() const noexcept { return ptr & tag_mask; }
+  bits_type bits() const noexcept { return value & tag_mask; }
 
   /// set all bits via integral type
   void bits(bits_type b) noexcept {
-    ptr &= ptr_mask;       // clear old bits
-    ptr |= (b & tag_mask); // set new bits
+    value &= ptr_mask;       // clear old bits
+    value |= (b & tag_mask); // set new bits
   }
 
   /// get bit at position pos
-  bool bit(unsigned pos) const noexcept { return ptr & (1 << pos); }
+  bool bit(unsigned pos) const noexcept { return value & (1 << pos); }
 
   /// set bit at position pos to value b
   void bit(unsigned pos, bool b) noexcept {
     BOOST_ASSERT(pos < Nbits);
     if (b)
-      ptr |= (1 << pos);
+      value |= (1 << pos);
     else
-      ptr &= ~(1 << pos);
+      value &= ~(1 << pos);
   }
 
   /// get raw pointer
-  pointer get() noexcept { return extract_ptr(ptr); }
+  pointer get() noexcept { return extract_ptr(value); }
 
   /// get raw pointer (const version)
-  const_pointer get() const noexcept { return extract_ptr(ptr); }
+  const_pointer get() const noexcept { return extract_ptr(value); }
 
   /// release ownership of raw pointer, bits remain intact
   pointer release() noexcept {
     auto tmp = get();
-    ptr &= ~ptr_mask; // nullify pointer bits
+    value &= ~ptr_mask; // nullify pointer bits
     return tmp;
   }
 
@@ -109,9 +116,9 @@ public:
   void reset(tagged_ptr p = tagged_ptr()) noexcept { p.swap(*this); }
 
   void swap(tagged_ptr &other) noexcept {
-    auto tmp = ptr;
-    ptr = other.ptr;
-    other.ptr = tmp;
+    auto tmp = value;
+    value = other.value;
+    other.value = tmp;
   }
 
   /// dereference operator
@@ -135,36 +142,38 @@ private:
   static constexpr bits_type tag_mask = ~ptr_mask;
 
   static bits_type alloc() {
-    return reinterpret_cast<bits_type>(
-        alignment::aligned_alloc(detail::pow2(Nbits), sizeof(Tpointee)));
+    return reinterpret_cast<bits_type>(alignment::aligned_alloc(
+        detail::max(detail::pow2(Nbits),
+                    alignment::alignment_of<Tpointee>::value),
+        sizeof(Tpointee)));
   }
 
-  static pointer extract_ptr(bits_type p) noexcept {
-    return reinterpret_cast<Tpointee *>(p & ptr_mask);
+  static pointer extract_ptr(bits_type v) noexcept {
+    return reinterpret_cast<Tpointee *>(v & ptr_mask);
   }
 
   friend bool operator==(const tagged_ptr &a, const tagged_ptr &b) noexcept {
-    return a.ptr == b.ptr;
+    return a.value == b.value;
   }
 
   friend bool operator!=(const tagged_ptr &a, const tagged_ptr &b) noexcept {
-    return a.ptr != b.ptr;
+    return a.value != b.value;
   }
 
   friend bool operator<(const tagged_ptr &a, const tagged_ptr &b) noexcept {
-    return a.ptr < b.ptr;
+    return a.value < b.value;
   }
 
   friend bool operator<=(const tagged_ptr &a, const tagged_ptr &b) noexcept {
-    return a.ptr <= b.ptr;
+    return a.value <= b.value;
   }
 
   friend bool operator>(const tagged_ptr &a, const tagged_ptr &b) noexcept {
-    return a.ptr > b.ptr;
+    return a.value > b.value;
   }
 
   friend bool operator>=(const tagged_ptr &a, const tagged_ptr &b) noexcept {
-    return a.ptr >= b.ptr;
+    return a.value >= b.value;
   }
 
   friend void swap(tagged_ptr &a, tagged_ptr &b) noexcept { a.swap(b); }
@@ -175,14 +184,14 @@ private:
   template <typename Upointee, unsigned Mbits, class... UArgs>
   friend tagged_ptr<Upointee, Mbits> make_tagged_ptr(UArgs &&...);
 
-  bits_type ptr;
+  bits_type value;
 };
 
 template <typename Tpointee, unsigned Nbits, class... TArgs>
 tagged_ptr<Tpointee, Nbits> make_tagged_ptr(TArgs &&... args) {
   tagged_ptr<Tpointee, Nbits> p;
-  p.ptr = p.alloc();
-  new (reinterpret_cast<Tpointee *>(p.ptr))
+  p.value = p.alloc();
+  new (reinterpret_cast<Tpointee *>(p.value))
       Tpointee(std::forward<TArgs>(args)...);
   return p;
 }
--- test/test_tagged_ptr.cpp
+++ test/test_tagged_ptr.cpp
@@ -2,8 +2,6 @@
 #include "boost/core/lightweight_test.hpp"
 #include "boost/utility/binary.hpp"
 
-static unsigned destructor_count = 0;
-
 int main() {
     using namespace boost::stateful_pointer;
 
@@ -14,21 +12,23 @@ int main() {
         BOOST_TEST_EQ(detail::make_ptr_mask(3), ~(1 | 2 | 4));
     }
 
+    static unsigned destructor_count_test_type = 0;
+
     struct test_type {
         int a; char b;
         test_type() : a(0), b(0) {}
         test_type(int x, char y) : a(x), b(y) {}
-        ~test_type() { ++destructor_count; }
+        ~test_type() { ++destructor_count_test_type; }
     };
 
     // check that tagged_ptr has the same size as void*
     BOOST_TEST_EQ(sizeof(tagged_ptr<test_type, 2>), sizeof(void*));
 
-    destructor_count = 0;
+    destructor_count_test_type = 0;
     {   // basic usage
         auto p = make_tagged_ptr<test_type, 2>(2, 3);
 
-        BOOST_TEST_EQ(!p, false);
+        BOOST_TEST(!!p);
         BOOST_TEST_EQ(static_cast<bool>(p), true);
         BOOST_TEST_EQ(p.bits(), BOOST_BINARY( 00 ));
         BOOST_TEST_EQ(p.bit(0), false);
@@ -61,36 +61,36 @@ int main() {
         p.reset();
         BOOST_TEST(p == (tagged_ptr<test_type, 2>()));
     }
-    BOOST_TEST_EQ(destructor_count, 1);
+    BOOST_TEST_EQ(destructor_count_test_type, 1);
 
-    destructor_count = 0;
+    destructor_count_test_type = 0;
     {   // null vs default constructed
-        auto p = tagged_ptr<test_type, 3>(); // nullptr
+        auto p = tagged_ptr<test_type, 3>(); // null
         BOOST_TEST(p == (tagged_ptr<test_type, 3>()));
         p.bits(BOOST_BINARY( 101 ));
-        BOOST_TEST_EQ(!p, true);
-        BOOST_TEST(p != (tagged_ptr<test_type, 3>()));
+        BOOST_TEST(!p); // still nullptr...
+        BOOST_TEST(p != (tagged_ptr<test_type, 3>())); // ... but not null
 
         auto q = make_tagged_ptr<test_type, 3>(); // default constructed
-        BOOST_TEST(q != (tagged_ptr<test_type, 3>()));
+        BOOST_TEST(q != (tagged_ptr<test_type, 3>())); // not nullptr
         q.bits(BOOST_BINARY( 101 ));
         BOOST_TEST_EQ(p.bits(), q.bits());
     }
-    BOOST_TEST_EQ(destructor_count, 1);
+    BOOST_TEST_EQ(destructor_count_test_type, 1);
 
     {   // release
-        destructor_count = 0;
+        destructor_count_test_type = 0;
         test_type* tp = nullptr;
         {
             auto p = make_tagged_ptr<test_type, 2>(2, 3);
             tp = p.release();
         }
-        BOOST_TEST_EQ(destructor_count, 0);
+        BOOST_TEST_EQ(destructor_count_test_type, 0);
         delete tp;
-        BOOST_TEST_EQ(destructor_count, 1);
+        BOOST_TEST_EQ(destructor_count_test_type, 1);
     }
 
-    destructor_count = 0;
+    destructor_count_test_type = 0;
     {
         auto p = make_tagged_ptr<test_type, 2>(2, 3);
         p.bit(0, false);
@@ -111,7 +111,64 @@ int main() {
         BOOST_TEST_EQ(r->a, 2);
         BOOST_TEST_EQ(r->b, 3);
     }
-    BOOST_TEST_EQ(destructor_count, 1);
+    BOOST_TEST_EQ(destructor_count_test_type, 1);
+
+    destructor_count_test_type = 0;
+    {   // swap
+        auto p = make_tagged_ptr<test_type, 3>(2, 3);
+        p.bits(BOOST_BINARY( 101 ));
+        auto q = make_tagged_ptr<test_type, 3>(4, 5);
+        q.bits(BOOST_BINARY( 010 ));
+
+        std::swap(p, q);
+
+        BOOST_TEST_EQ(p.bits(), BOOST_BINARY( 010 ));
+        BOOST_TEST_EQ(p->a, 4);
+        BOOST_TEST_EQ(p->b, 5);
+        BOOST_TEST_EQ(q.bits(), BOOST_BINARY( 101 ));
+        BOOST_TEST_EQ(q->a, 2);
+        BOOST_TEST_EQ(q->b, 3);
+
+        p.swap(q);
+
+        BOOST_TEST_EQ(p.bits(), BOOST_BINARY( 101 ));
+        BOOST_TEST_EQ(p->a, 2);
+        BOOST_TEST_EQ(p->b, 3);
+        BOOST_TEST_EQ(q.bits(), BOOST_BINARY( 010 ));
+        BOOST_TEST_EQ(q->a, 4);
+        BOOST_TEST_EQ(q->b, 5);
+    }
+    BOOST_TEST_EQ(destructor_count_test_type, 2);
+
+    static unsigned destructor_count_base = 0;
+    struct base {
+        int a = 1;
+        virtual ~base() { ++destructor_count_base; }
+    };
+
+    static unsigned destructor_count_derived = 0;
+    struct derived : public base {
+        char b = 2;
+        virtual ~derived() { ++destructor_count_derived; }
+    };
+
+    struct derived2 : public base {};
+
+    {   // conversion derived <-> base
+        auto d = make_tagged_ptr<derived, 3>();
+        d.bits(BOOST_BINARY( 101 ));
+        tagged_ptr<base, 3> b = std::move(d);
+        BOOST_TEST_EQ(b.bits(), BOOST_BINARY( 101 ));
+        BOOST_TEST_EQ(b->a, 1);
+        auto dp = dynamic_cast<derived*>(b.get());
+        BOOST_TEST(dp);
+        BOOST_TEST_EQ(dp->a, 1);
+        BOOST_TEST_EQ(dp->b, 2);
+        auto d2p = dynamic_cast<derived2*>(b.get());
+        BOOST_TEST(!d2p);
+    }
+    BOOST_TEST_EQ(destructor_count_base, 1);
+    BOOST_TEST_EQ(destructor_count_derived, 1);
 
     return boost::report_errors();
 }
